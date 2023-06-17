a217dd68e7ac3ed6ced79e295c6a96cae9e8d43e
chijazz <chijazzconcepts@gmail.com>
2023-06-17 08:27:55 +0100

    new

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,49 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Backend = () => {
   return (
-    <div className='skills_content'>
-    <h3 className='skills_title'>
-Backend 
-    </h3>
-    <div className='skills_box'>
-        <div className='skills_group'>
-            <div className='skills_data'>
-                <i className='bx bx-badge-check'></i>
-                <div>
-                    <h3 className='skills_name'>Node Js</h3>
-                    <span className='skills_level'>Basic</span>
-                </div>
+    <div className="skills_content">
+      <h3 className="skills_title">Backend</h3>
+      <div className="skills_box">
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Node Js</h3>
+              <span className="skills_level">Basic</span>
             </div>
-            <div className='skills_data'>
-                <i className='bx bx-badge-check'></i>
-                <div>
-                    <h3 className='skills_name'>MongoDB</h3>
-                    <span className='skills_level'>Intermediate</span>
-                </div>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">MongoDB</h3>
+              <span className="skills_level">Intermediate</span>
             </div>
-            {/* <div className='skills_data'>
-                <i className='bx bx-badge-check'></i>
-                <div>
-                    <h3 className='skills_name'>Firebase</h3>
-                    <span className='skills_level'>Intermediate</span>
-                </div>
-            </div> */}
-            <div className='skills_data'>
-                <i className='bx bx-badge-check'></i>
-                <div>
-                <h3 className='skills_name'>Firebase</h3>
-                    <span className='skills_level'>Intermediate</span>
-                </div>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">PHP</h3>
+              <span className="skills_level">Intermediate</span>
             </div>
-            {/* <div className='skills_data'>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Firebase</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+          {/* <div className='skills_data'>
                 <i className='bx bx-badge-check'></i>
                 <div>
                 <h3 className='skills_name'>Firebase</h3>
                     <span className='skills_level'>Intermediate</span>
                 </div>
             </div> */}
-            {/* <div className='skills_data'>
+          {/* <div className='skills_data'>
                 <i className='bx bx-badge-check'></i>
                 <div>
                 <h3 className='skills_name'>Firebase</h3>
@@ -51,9 +49,9 @@ Backend
                 </div>
             </div> */}
         </div>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Backend
+export default Backend;
